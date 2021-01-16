d19ad0089ccf315403df52dd99b80d8cf6e85980
Jason Ray <jasonray@jayray.net>
2021-01-16 14:52:02 -0500

    #6: implement basic logging

--- src/main/java/net/jayray/pepper/leagueage/Application.java
+++ src/main/java/net/jayray/pepper/leagueage/Application.java
@@ -2,6 +2,8 @@ package net.jayray.pepper.leagueage;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+	private Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -19,14 +23,13 @@ public class Application {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			logger.trace("Inspect beans provided by Spring Boot application:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
-				System.out.println(beanName);
+				logger.trace("beanName: [{}]", beanName);
 			}
-
 		};
 	}
 
--- src/main/java/net/jayray/pepper/leagueage/HelloController.java
+++ src/main/java/net/jayray/pepper/leagueage/HelloController.java
@@ -1,14 +1,19 @@
 package net.jayray.pepper.leagueage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.debug("Hello Controller invoked");
+        return "Greetings from Spring Boot!";
+    }
 
 }

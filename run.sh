994180c16905318d66a211e1b64bd93104d48541
Jason Ray <jasonray@jayray.net>
2021-01-16 14:29:13 -0500

    ./run.sh for launching the application

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "\n\n\n\n${RED}Building API${NC}\n\n\n\n"
+mvn install
+if [ $? -ne 0 ]
+then
+  echo "Build failed" >&2
+  exit $?
+fi
+
+printf "\n\n\n\n${RED}Running API${NC}\n\n\n\n"
+./mvnw spring-boot:run

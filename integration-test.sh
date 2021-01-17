b14a0a519309e46a4444bce47f5af014dc395169
Jason Ray <jasonray@jayray.net>
2021-01-16 19:05:06 -0500

    #2 script to kick off integration tests

---
+++ integration-test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+### GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+printf "\n\n\n\n${RED}Running API Integration Tests${NC}\n\n\n\n"
+mvn clean test -Dcucumber.glue="integration.test" -Dcucumber.plugin="pretty, html:target/integration-test/integration-test-results.html" -Dcucumber.features="src/test/resources/features/" -Dcucumber.filter.tags="not @ignored" -Dcucumber.publish.quiet=true
